46912e0997487296d11bc31dbcf9f49503a3fb80
Nigel Hurnell <admin@hurnell.com>
2026-02-28 21:52:13 +0100

    remove comments

--- app/src/main/java/apk/hurnell/recipebookreader/ui/AppLifecycleListener.kt
+++ app/src/main/java/apk/hurnell/recipebookreader/ui/AppLifecycleListener.kt
@@ -7,12 +7,10 @@ import apk.hurnell.recipebookreader.RecipeBookReaderApplication
 class AppLifecycleListener(private val app: RecipeBookReaderApplication) : DefaultLifecycleObserver {
 
     override fun onStart(owner: LifecycleOwner) {
-        // App comes to foreground
         println("App is in the FOREGROUND")
     }
 
     override fun onStop(owner: LifecycleOwner) {
-        // App goes to background (Home button or Recents clicked)
         val lastActivity = app.lastActiveActivityName
 
         println("App went to background. The user was looking at: $lastActivity")
