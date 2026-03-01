70fc2f4e963c3cc5bb4d6f67690f669585db998d
Nigel Hurnell <admin@hurnell.com>
2026-03-01 17:35:56 +0100

    book location needed

--- app/src/main/java/apk/hurnell/recipebookreader/model/BaseBookmarkTocItem.kt
+++ app/src/main/java/apk/hurnell/recipebookreader/model/BaseBookmarkTocItem.kt
@@ -4,6 +4,7 @@ interface  BaseBookmarkTocItem {
      val tocId: Long?
      val bookId: Long?
      val bookTitle: String?
+     val bookLocation: String?
      val page: Int
      val offset: Float
      val scale: Float
