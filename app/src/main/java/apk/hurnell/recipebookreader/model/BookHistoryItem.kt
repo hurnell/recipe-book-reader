c5c577a12fa39bbc4b46df3ce65900129e730fbd
Nigel Hurnell <admin@hurnell.com>
2026-03-04 08:14:55 +0100

    possibly null

--- app/src/main/java/apk/hurnell/recipebookreader/model/BookHistoryItem.kt
+++ app/src/main/java/apk/hurnell/recipebookreader/model/BookHistoryItem.kt
@@ -1,10 +1,10 @@
 package apk.hurnell.recipebookreader.model
 
 data class BookHistoryItem(
-    val id: Long? = null,
-    val bookId: Long,
-    val page: Int,
-    val offset: Int?,
-    val translationX: Float?,
-    val scale: Float?
+    var id: Long? = null,
+    var bookId: Long? = null,
+    var page: Int,
+    var offset: Int? = null,
+    var translationX: Float?,
+    var scale: Float?
 )
