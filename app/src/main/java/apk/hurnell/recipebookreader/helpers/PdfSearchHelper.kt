472d5deb14e0def9b71e6a95360fcdeab2145652
Nigel Hurnell <admin@hurnell.com>
2026-04-08 08:15:10 +0200

    check with and without normalisation

--- app/src/main/java/apk/hurnell/recipebookreader/helpers/PdfSearchHelper.kt
+++ app/src/main/java/apk/hurnell/recipebookreader/helpers/PdfSearchHelper.kt
@@ -22,7 +22,7 @@ data class SearchResult(
     val coordinates: Coordinates
 )
 
-class PdfSearchHelper(private val document: Document,private val  repository: PdfRepository) {
+class PdfSearchHelper(private val document: Document, private val repository: PdfRepository) {
 
     fun search(query: String, startPage: Int = 0): Flow<SearchResult> = flow {
         val pageCount = document.countPages()
@@ -30,7 +30,7 @@ class PdfSearchHelper(private val document: Document,private val  repository: Pd
         for (i in 0 until pageCount) {
             val currentIndex = (startPage + i) % pageCount
 
-            val result = processPage(currentIndex, query,)
+            val result = processPage(currentIndex, query)
 
             if (result != null) {
                 emit(result)
@@ -55,9 +55,10 @@ class PdfSearchHelper(private val document: Document,private val  repository: Pd
                         val lineBuilder = StringBuilder()
                         line.chars?.forEach { char -> lineBuilder.append(char.c.toChar()) }
                         val lineText = lineBuilder.toString().trim()
-                        val normalizedLine = repository.normalizeText( lineText)
-
-                        if (normalizedLine.contains(query, ignoreCase = true)) {
+                        val isInLine = lineText.contains(query, ignoreCase = true)
+                        val normalizedLine = repository.normalizeText(lineText)
+                        val isInNormalised = normalizedLine.contains(query, ignoreCase = true)
+                        if (isInLine || isInNormalised) {
                             matchingLines.add(lineText)
                             foundRectangles.add(line.bbox)
                         }
